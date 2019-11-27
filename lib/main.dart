d5f8760ae6718bb53d42cf8f96832057f2b6bcb6
MAC <mac@Foluwa.local>
2019-11-27 14:35:01 +0100

    Ipushed the static version of the flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: NinjaCard(),
-));
+      home: NinjaCard(),
+    ));
 
 class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title:  Text('Developer ID Card'),
+        title: Text('Developer ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/image.jpeg'),
+                  radius: 50.0,
+                ),
+              ),
+              Divider(
+                height: 90.0,
+                color: Colors.grey[600],
+              ),
+              Text('NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(height: 10.0),
+              Text('Foluwa',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(height: 30.0),
+              Text('ROLE',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(height: 10.0),
+              Text('Full Stack',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'foluwa@com.ng',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
